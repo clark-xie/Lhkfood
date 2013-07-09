5f279ea6f958e8f5d4b6ae2a6b7123ae00046133
huwei <EsriApp@sumomo-rais-MacBook-Pro.local>
2013-07-09 13:35:43 +0800

    Initial Commit

---
+++ DigitHubei_IPad/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DigitHubei_IPad
+//
+//  Created by huwei on 7/9/13.
+//  Copyright (c) 2013 huwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DigitHubei_IPad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DigitHubei_IPad
+//
+//  Created by huwei on 7/9/13.
+//  Copyright (c) 2013 huwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
