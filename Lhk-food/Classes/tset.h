85e82cc4eb107baf13e55ea8cd68e03f127ed2f1
happyxiechao <hh.xiechao@gmail.com>
2015-07-02 16:39:03 +0800

    add some lost file

---
+++ Lhk-food/Classes/tset.h
@@ -0,0 +1,13 @@
+//
+//  tset.h
+//  Lhk-food
+//
+//  Created by 谢超 on 15/7/2.
+//  Copyright (c) 2015年 huwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface tset : NSObject
+
+@end
---
+++ Lhk-food/Classes/tset.m
@@ -0,0 +1,13 @@
+//
+//  tset.m
+//  Lhk-food
+//
+//  Created by 谢超 on 15/7/2.
+//  Copyright (c) 2015年 huwei. All rights reserved.
+//
+
+#import "tset.h"
+
+@implementation tset
+
+@end
